7fac2f9c59c7bdb6601326840759629601bf3b1b
Hampus Reis <hampus.reis@diakrit.com>
2025-04-19 09:29:50 +0700

    refactor: update package title and improve uninstall error handling

--- tools/chocolateybeforemodify.ps1
+++ tools/chocolateybeforemodify.ps1
@@ -1,3 +1,4 @@
-﻿# Stop and delete the service if it exists
+﻿$ErrorActionPreference = 'Stop'
+# Stop and delete the service if it exists
 sc.exe stop fluent-bit
 sc.exe delete fluent-bit
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,3 @@
-
-
 $ErrorActionPreference = 'Stop'
 
 $process = Get-Process "fluent-bit*" -ea 0
@@ -31,24 +29,29 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs
 
+# Get the installation directory from the registry
+if (Test-Path "HKLM:\SOFTWARE\WOW6432Node\Chronosphere Inc.\fluent-bit") {
+  Write-Debug "64-bit version installed"
+  $regKey = Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Chronosphere Inc.\fluent-bit"
+  $regKeySelector = $regKey | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Select-Object -First 1
+  $installDir = $regKey.$regKeySelector
+  Write-Host "Fluentbit is installed at: $installDir"
+} elseif (Test-Path "HKLM:\SOFTWARE\Chronosphere Inc.\fluent-bit") {
+  Write-Debug "32-bit version installed"
+  $regKey = Get-ItemProperty "HKLM:\SOFTWARE\Chronosphere Inc.\fluent-bit"
+  $regKeySelector = $regKey | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Select-Object -First 1
+  $installDir = $regKey.$regKeySelector
+  Write-Host "Fluentbit is installed at: $installDir"
+} else {
+  Write-Error "Fluentbit installation directory not found in registry."
+  exit 1
+}
 
 $ServiceName = "fluent-bit"
-$ExecutablePath = "C:\Program Files\fluent-bit\bin\fluent-bit.exe"
-$ConfigPath = "C:\Program Files\fluent-bit\conf\fluent-bit.conf"
+$ExecutablePath = Join-Path -Path $installDir -ChildPath "bin\fluent-bit.exe"
+$ConfigPath = Join-Path -Path $installDir -ChildPath "conf\fluent-bit.conf"
 
-$cmdFilePath = "$toolsdir\fluent-bit.cmd"
-    $cmdFileContent = @"
-sc.exe create $ServiceName binpath= "\"$ExecutablePath\" -c \"$ConfigPath\"" start= auto
-sc.exe description $ServiceName "This service runs Fluent Bit, a log collector that enables real-time processing and delivery of log data to centralized logging systems."
-sc.exe start $ServiceName
-"@
-    Set-Content -Path $cmdFilePath -Value $cmdFileContent -Force
+New-Service $ServiceName -BinaryPathName "`"$ExecutablePath`" -c `"$ConfigPath`"" -StartupType Automatic -Description "This service runs Fluent Bit, a log collector that enables real-time processing and delivery of log data to centralized logging systems."
+Start-Service -Name $ServiceName
 
-try {
-    Start-Process -FilePath $cmdFilePath -Wait
-}
-catch {
-    Write-Error $_.Exception.Message
-    exit 1
-}
 Write-Host "Fluent Bit service created successfully." -ForegroundColor Green
