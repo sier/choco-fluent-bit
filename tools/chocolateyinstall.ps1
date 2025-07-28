a2b7460d332c4c06fd275ba377bb731dd283d2fd
Hampus Reis <hampus.reis@diakrit.com>
2025-07-29 00:50:40 +0700

    bump version to 4.0.5

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ if ($process) {
 }
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://packages.fluentbit.io/windows/fluent-bit-4.0.4-win32.exe'
-$url64      = 'https://packages.fluentbit.io/windows/fluent-bit-4.0.4-win64.exe'
+$url        = 'https://packages.fluentbit.io/windows/fluent-bit-4.0.5-win32.exe'
+$url64      = 'https://packages.fluentbit.io/windows/fluent-bit-4.0.5-win64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -19,9 +19,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'fluent-bit*'
-  checksum      = 'E4C6FA0555E793342DAAD954F5EDEA5801D0D44904E31E2F8778FEC5B0FFCFD2'
+  checksum      = '31d1d98f3c9a8cb829abc448de93ef859b63d02852c8529482fe284ed1176356'
   checksumType  = 'sha256'
-  checksum64    = '3B51C5FE30859CA1D6959F90C37B3C7B23AEC1641CA7068C458F900FE8B0A8E9'
+  checksum64    = '935bc64cd81140e37c3e59aeed65e96a5e2c1e255252d2084754f2e829f04301'
   checksumType64= 'sha256'
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
